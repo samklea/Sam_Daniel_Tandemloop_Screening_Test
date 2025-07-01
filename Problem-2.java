27e0f80f70d2edf6746806fbc63103b7c865a5f7
samklea <83422640+samklea@users.noreply.github.com>
2025-07-01 15:38:11 +0530

    Problem-2.java

--- Problem-2.java
+++ Problem-2.java
@@ -1,1 +1,15 @@
+import java.util.Scanner;
 
+public class Problem2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int num = 1;
+
+        for (int i = 0; i < x; i++) {
+            System.out.print(num + " ");
+            num += 2;
+        }
+        sc.close();
+    }
+}
