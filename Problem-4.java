b63cdb17adff7d2ff50ec35e1e3a6d1a0cc02075
samklea <83422640+samklea@users.noreply.github.com>
2025-07-01 15:40:17 +0530

    Create Problem-4.java
    Count multiples in list for digits 1–9

---
+++ Problem-4.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Problem4 {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 8, 9, 12, 46, 76, 82, 15, 20, 30};
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 1; i <= 9; i++) {
+            map.put(i, 0);
+        }
+
+        for (int num : arr) {
+            for (int i = 1; i <= 9; i++) {
+                if (num % i == 0) {
+                    map.put(i, map.get(i) + 1);
+                }
+            }
+        }
+
+        System.out.println(map);
+    }
+}
